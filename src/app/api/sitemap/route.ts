7e2c8893e6e21f4d8350bc7f70d88baa0f0e2eb0
kenseibaricata <kensei@baricata.com>
2025-07-04 15:00:31 +0900

    feat: sitemap.xmlをAPI Routeで動的生成し、rewritesでルーティング

---
+++ src/app/api/sitemap/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n  <url>\n    <loc>https://www.sourire.win/</loc>\n    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>\n  </url>\n</urlset>`;
+
+  return new NextResponse(sitemap, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+      'Cache-Control': 'no-store',
+    },
+  });
+} 
