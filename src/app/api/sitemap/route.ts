42fe7cc5c1848743a1c9e4639f8fb587e5b6143c
kenseibaricata <kensei@baricata.com>
2025-07-04 15:08:44 +0900

    fix: sitemap.xmlのXML宣言を1行目に厳密に配置

--- src/app/api/sitemap/route.ts
+++ src/app/api/sitemap/route.ts
@@ -1,7 +1,14 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n  <url>\n    <loc>https://www.sourire.win/</loc>\n    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>\n  </url>\n</urlset>`;
+  const sitemap =
+    '<?xml version="1.0" encoding="UTF-8"?>\n' +
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n' +
+    '  <url>\n' +
+    '    <loc>https://www.sourire.win/</loc>\n' +
+    `    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>\n` +
+    '  </url>\n' +
+    '</urlset>';
 
   return new NextResponse(sitemap, {
     status: 200,
